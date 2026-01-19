985a10e6af8a4bed04de9cbdfe614ed0e99f8ff0
xdecker <xavier_deckza@hotmail.com>
2026-01-19 00:48:03 -0500

    feat: notfound component

---
+++ src/app/shared/components/not-found/not-found.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-not-found',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.css',
+})
+export class NotFoundComponent {}
