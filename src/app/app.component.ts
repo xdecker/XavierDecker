faf7414a9bc03de043f1cd856802c18e960e32dd
xdecker <xavier_deckza@hotmail.com>
2026-01-18 19:48:24 -0500

    feat: add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastComponent } from './core/components/toast/toast.component';
 import { ToastService } from './core/services/toast.service';
+import { HeaderPageComponent } from "./shared/components/header-page/header-page.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ToastComponent],
+  imports: [RouterOutlet, ToastComponent, HeaderPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/shared/components/header-page/header-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-page',
+  standalone:true,
+  imports: [],
+  templateUrl: './header-page.component.html',
+  styleUrl: './header-page.component.css'
+})
+export class HeaderPageComponent {
+  constructor(){
+  }
+  
+}
