277f2fd48ab77766dc93d1be5b00e98eab959c33
xdecker <xavier_deckza@hotmail.com>
2026-01-19 00:47:55 -0500

    feat: Add general error page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './features/products/presentation/pages/product-list/product-list.component';
 import { ProductFormComponent } from './features/products/presentation/pages/product-form/product-form.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 export const routes: Routes = [
   {
@@ -19,5 +20,10 @@ export const routes: Routes = [
   {
     path: 'products/edit/:id',
     component: ProductFormComponent,
+  },
+
+  {
+    path: '**',
+    component: NotFoundComponent,
   },
 ];
